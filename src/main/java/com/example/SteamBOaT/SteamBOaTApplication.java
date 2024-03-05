4d8e1171d9793d630b5b746da8dff356bb71d85e
Afonin Alexei <aleksafonin2@mail.ru>
2024-03-05 21:10:36 +0300

    Add files via upload

---
+++ src/main/java/com/example/SteamBOaT/SteamBOaTApplication.java
@@ -0,0 +1,13 @@
+package com.example.SteamBOaT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamBOaTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamBOaTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SteamBOaT/SteamBOaTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SteamBOaT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamBOaTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
